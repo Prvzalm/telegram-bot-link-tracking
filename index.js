ecd920390d7a9e685c7045a2dfb4bca584f5089d
Parvez Alam <prvzalm69@gmail.com>
2023-12-21 23:53:44 +0530

    chatMemberUpdated #(try-2)

--- index.js
+++ index.js
@@ -1,63 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
 
-const token = process.env.TOKEN; // Replace with your actual bot token
-const bot = new TelegramBot(token, { polling: true });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// In-memory storage to keep track of counts
-const linkCounts = {};
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/telegram_tracking', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Create a schema for tracking information
+const trackingSchema = new mongoose.Schema({
+    inviteLink: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    joinedCount: {
+        type: Number,
+        default: 0,
+    },
+    leftCount: {
+        type: Number,
+        default: 0,
+    },
+});
+
+// Create a model for tracking information
+const Tracking = mongoose.model('Tracking', trackingSchema);
+
+// Set up the Telegram bot
+const botToken = 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(botToken, { polling: true });
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
 
 // Event handler for updates in chat members
 bot.on('chatMemberUpdated', async (update) => {
     const chatId = update.chat.id;
-    const userId = update.from.id;
+    const inviteLink = getInviteLinkFromChat(update.chat);
     const status = update.new_chat_member.status; // 'member' or 'left' or 'kicked'
 
-    if (status === 'member') {
-        // User joined the chat
-        handleUserJoined(chatId, userId);
-    } else if (status === 'left' || status === 'kicked') {
-        // User left or was kicked from the chat
-        handleUserLeft(chatId, userId);
+    try {
+        let trackingInfo = await Tracking.findOne({ inviteLink });
+
+        if (!trackingInfo) {
+            trackingInfo = new Tracking({ inviteLink });
+        }
+
+        if (status === 'member') {
+            // User joined the chat
+            trackingInfo.joinedCount += 1;
+        } else if (status === 'left' || status === 'kicked') {
+            // User left or was kicked from the chat
+            trackingInfo.leftCount += 1;
+        }
+
+        await trackingInfo.save();
+    } catch (error) {
+        console.error('Error updating tracking information:', error.message);
     }
 });
 
-// Periodically check and update counts for invite links
-setInterval(() => {
-    for (const link in linkCounts) {
-        getInviteLinkMembers(link);
-    }
-}, 5000); // Adjust the interval as needed (in milliseconds)
-
-// Helper function to handle user joining
-function handleUserJoined(chatId, userId) {
-    // Implement your logic here
-    console.log(`User ${userId} joined the chat ${chatId}`);
+// Helper function to extract the invite link from the chat
+function getInviteLinkFromChat(chat) {
+    // Implement your logic to extract the invite link from the chat
+    // For simplicity, let's assume the link is a URL in the chat's description
+    return chat.description || '';
 }
 
-// Helper function to handle user leaving
-function handleUserLeft(chatId, userId) {
-    // Implement your logic here
-    console.log(`User ${userId} left or was kicked from the chat ${chatId}`);
-}
-
-// Helper function to get invite link members and update counts
-async function getInviteLinkMembers(link) {
-    try {
-        const response = await bot.getInviteLinkMembers(link);
-        const chatId = response.chat.id;
-        const members = response.members;
-
-        // Update counts based on the members list
-        const joinedCount = members.filter((member) => member.status === 'member').length;
-        const leftCount = members.filter((member) => member.status === 'left' || member.status === 'kicked').length;
-
-        // Update in-memory storage
-        linkCounts[link] = { joined: joinedCount, left: leftCount };
-
-        // Send a message or perform further actions if needed
-        console.log(`Invite link ${link}: Joined: ${joinedCount}, Left: ${leftCount}`);
-    } catch (error) {
-        console.error('Error getting invite link members:', error.message);
-    }
-}
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
